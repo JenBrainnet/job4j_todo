b43abdae7eab78f736781a1e364527ff6ca3b07d
JenBrainnet <benebrak01@gmail.com>
2026-04-30 10:30:55 +0300

    Add Task model and Hibernate configuration

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,28 @@
+package ru.job4j.todo.model;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String description;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm")
+    private LocalDateTime created = LocalDateTime.now();
+
+    private boolean done = false;
+
+}
