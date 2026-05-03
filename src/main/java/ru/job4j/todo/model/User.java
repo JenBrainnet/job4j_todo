eeeff62c36d7d12a107f964bb271de0a1f2efd14
JenBrainnet <benebrak01@gmail.com>
2026-05-04 00:17:58 +0300

    Add users schema and User model

---
+++ src/main/java/ru/job4j/todo/model/User.java
@@ -0,0 +1,28 @@
+package ru.job4j.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String login;
+
+    private String password;
+
+}
